8a883c9d0d75223726cdf25995584da9489cf7cc
Mattiz Lowagie <121019142+mattiz2010@users.noreply.github.com>
2024-12-15 13:00:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 let img1 = document.getElementById("image1");
 let dev1 = document.getElementById("website1");
+let img3 = document.getElementById("image3");
 
-img1.onclick = imageButton1; // This should call imageButton1, not imgageButton1
+img1.onclick = imageButton1;
+img3.onclick = imageButton3;
 
 function imageButton1() {
-	// Corrected typo here
 	dev1.style.visibility = "hidden";
+}
+
+function imageButton3() {
+	window.location.href = "https://finalprojectmattiz.netlify.app";
 }
